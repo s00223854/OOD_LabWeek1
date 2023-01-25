b145cea34b762fe7a2b3a7b37ff6f14ba6c9adca
Eoin Kilgallon <S00223854@atu.ie>
2023-01-25 11:17:11 +0000

    Band Names on List Box Displayed

---
+++ OOD_LabWeek1/Band.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_LabWeek1
+{
+    public class Band :IComparable
+    {
+        //properties
+        public int bandDateFormed { get; set; }
+
+        public string memberName { get; set; }
+
+        public string bandName { get; set; }
+
+        //constructers
+        public Band(int banddateformed,string membername, string bandname)
+        {
+            bandDateFormed = banddateformed;
+            memberName = membername;
+            bandName = bandname;
+        }
+
+        public Band()
+        {
+
+        }
+
+        //methods
+
+        public override string ToString()
+        {
+            return bandName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Band otherBand = obj as Band;
+
+            return this.bandName.CompareTo(otherBand.bandName);
+        }
+    }
+}
--- OOD_LabWeek1/MainWindow.xaml.cs
+++ OOD_LabWeek1/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace OOD_LabWeek1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> allBands = new List<Band>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Band b1 = new Band() { bandName = "Green Day",bandDateFormed =1986 , memberName = "Billie Joe Armstrong, Mike Dirnt, Tre Cool"};
+            Band b2 = new Band() { bandName = "AC/DC", bandDateFormed = 1973, memberName = "Brian Johnson, Bon Scott, Dave Evans, Axl Rose"};
+            Band b3 = new Band() { bandName = "The White Stripes", bandDateFormed = 1997, memberName = "Jack White, Meg White" };
+            Band b4 = new Band() { bandName = "The Beatles", bandDateFormed = 1960, memberName = "John Lennon, Ringo Starr, Paul McCartney, George Harrison" };
+            Band b5 = new Band() { bandName = "My Chemical Romance", bandDateFormed = 2001, memberName = "Gerard Way, Ray Toro, Mikey Way, Frank Iero" };
+            Band b6 = new Band() { bandName = "Daft Punk", bandDateFormed = 1993, memberName = "Guy-Manuel de Homem-Christo, Thomas Bangalter" };
+
+            allBands.Add(b1);
+            allBands.Add(b2);
+            allBands.Add(b3);
+            allBands.Add(b4);
+            allBands.Add(b5);
+            allBands.Add(b6);
+
+            LtBxBandNames.ItemsSource = allBands;
         }
     }
 }
